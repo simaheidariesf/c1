55448711dee8f9f3c28ba3ddf5b608b99f139b34
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:59:59 +0430

    add IProjectService interface- 1401/02/18 23:45

---
+++ C1System.Core/Services/Interface/IProjectService.cs
@@ -0,0 +1,18 @@
+﻿using C1System.DataLayar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1System.Core.Services.Interface
+{
+    public interface IProjectService
+    {
+        List<Project> GetAllProject();
+        Project GetProjectById(Guid id);
+        bool AddProject(Project project);
+        bool DeleteProject(Project project);
+        bool UpdateProject(Project project);
+    }
+}
