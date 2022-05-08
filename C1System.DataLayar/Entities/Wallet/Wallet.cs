9eef716d93d83cc8d21c93d33fef4419bd58a927
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:52:48 +0430

    add Wallet entity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/Wallet/Wallet.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities.Wallet;
+
+public class Wallet
+{
+    [Key]
+    public int WalletId { get; set; }
+    
+    
+}
