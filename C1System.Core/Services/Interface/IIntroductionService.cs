ee1c8a4f09c8dc5fafcf35b2e160b47e5741874c
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:59:25 +0430

    add IIntroductionService interface- 1401/02/18 23:45

---
+++ C1System.Core/Services/Interface/IIntroductionService.cs
@@ -0,0 +1,18 @@
+﻿using C1System.DataLayar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1System.Core.Services.Interface
+{
+    public interface IIntroductionService
+    {
+        List<Introduction> GetAllIntroduction();
+        Introduction GetIntroductionById(Guid id);
+        bool AddIntroduction(Introduction introduction);
+        bool DeleteIntroduction(Introduction introduction);
+        bool UpdateIntroduction(Introduction introduction);
+    }
+}
