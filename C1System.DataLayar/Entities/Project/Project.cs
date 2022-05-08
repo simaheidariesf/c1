b590c716ff3b2c3e9cf8c6d3e6da2d6b4ea6553f
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:47:33 +0430

    add Project entity - 1401/02/18  23:45

---
+++ C1System.DataLayar/Entities/Project/Project.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities;
+
+public class Project
+{
+    [Key]
+    public Guid ProjectId { get; set; }
+
+    [Display(Name = "تصویر")]
+    public string Picture { get; set; }
+    
+    [Display(Name ="عنوان پروژه")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [MinLength(20 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(150 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Title { get; set; }
+
+    [Display(Name = "عنوان فرعی پروژه")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
+    [MinLength(80, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(350, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string SubTitle { get; set; }
+    
+    [Display(Name = "توضیحات پروژه")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
+    [MinLength(200, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(1000, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Desc { get; set; }
+
+    public bool IsDelete { get; set; }
+    
+    [Display(Name = "تصاویر")]
+    public string Media { get; set; }
+
+}
