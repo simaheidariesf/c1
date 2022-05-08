ac3698c10a655970e10dd11fe317ecc2c9aa844b
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:51:05 +0430

    add TypeOfActivity entity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/User/TypeOfActivity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities;
+
+public class TypeOfActivity
+{
+    [Key]
+    public Guid TypeOfActivityId { get; set; }
+    
+    [Display(Name ="نوع فعالیت شما")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [MinLength(20 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(300 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Title { get; set; }
+}
