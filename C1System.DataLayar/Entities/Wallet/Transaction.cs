14e02352fba2d227e2e0a25edf129820795539dd
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:53:05 +0430

    add Transaction entity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/Wallet/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System.DataLayar.Entities.Wallet;
+
+public class Transaction
+{
+    [Key]
+    public int TransactionId { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime CreateDateTime { get; set; }
+        
+    public decimal Amount { get; set; }
+    
+    [Required]
+    [StringLength(200)]
+    public string Title { get; set; }
+    
+    [Column("OK")]
+    public bool Ok { get; set; } = false;
+    
+}
