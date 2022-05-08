d6f59f2a321390c32207554a92f2e251cbc6ed84
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-09 00:00:42 +0430

    add IntroductionService interface- 1401/02/18 23:45

---
+++ C1System.Core/Services/IntroductionService.cs
@@ -0,0 +1,85 @@
+﻿using C1System.Core.Services.Interface;
+using C1System.DataLayar.Context;
+using C1System.DataLayar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1System.Core.Services
+{
+    public class IntroductionService : IIntroductionService
+    {
+        private readonly C1SystemContext _context;
+        public IntroductionService(C1SystemContext context)
+        {
+            _context = context;
+        }
+
+        public bool AddIntroduction(Introduction introduction)
+        {
+            try
+            {
+                _context.Introductions.Add(introduction);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool DeleteIntroduction(Introduction introduction)
+        {
+            if (introduction != null)
+            {
+                try
+                {
+                    _context.Introductions.Remove(introduction);
+                    _context.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    return false;
+                }
+            }
+            else
+                return false;
+        }
+
+        public List<Introduction> GetAllIntroduction()
+        {
+            return _context.Introductions.ToList();
+        }
+
+        public Introduction GetIntroductionById(Guid id)
+        {
+            return _context.Introductions.Find(id);
+        }
+
+        public bool UpdateIntroduction(Introduction introduction)
+        {
+            if (introduction != null)
+            {
+                try
+                {
+                    _context.Introductions.Update(introduction);
+                    _context.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            else
+                return false;
+        }
+    }
+}
