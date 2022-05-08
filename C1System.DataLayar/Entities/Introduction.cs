33ab0c8635f90ef6726115c4508e788025176c20
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:48:07 +0430

    add Introduction entity - 1401/02/18  23:45

---
+++ C1System.DataLayar/Entities/Introduction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1System.DataLayar.Entities
+{
+    public class Introduction
+    {
+        [Key]
+        public Guid IntroductionId { get; set; }
+
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
+        [MinLength(15, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
+        [MaxLength(200, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+        public string Title { get; set; }
+        
+        [Display(Name = "عنوان فرعی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
+        [MinLength(30, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
+        [MaxLength(200, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+        public string? SubTitle { get; set; }
+        
+        [Display(Name = "توضیحات")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
+        public string Desc { get; set; }
+        
+        [Display(Name = "ویدئو")]
+        public string Video { get; set; }
+        
+        [Display(Name = "تصویر کاور ویدئو")]
+        public string? VideoCoverImage { get; set; }
+    }
+}
