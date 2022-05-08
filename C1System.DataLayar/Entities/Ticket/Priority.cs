adcfb3a00252032ba322715df207dfa1a3448d37
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:50:16 +0430

    add Priority entity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/Ticket/Priority.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities.Ticket;
+
+public class Priority
+{
+    [Key] 
+    public int PriorityId { get; set; }
+    
+    [Display(Name = "عنوان تیکت")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(20, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string PriorityTitle { get; set; }
+}
