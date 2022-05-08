6ca4c5d44ec220b8ddd6aa3071c5226fcc2e9253
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:57:59 +0430

    add BaseEntity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/Base/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System.DataLayar.Entities.Base;
+
+public class BaseEntity
+{
+    public BaseEntity()
+    {
+        CreatedAt = DateTime.Now;
+    }
+    
+    [Key]
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    [Required]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    public DateTime? DeletedAt { get; set; }
+}
