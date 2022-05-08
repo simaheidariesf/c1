2a03f6571f6faeba50a85aeaa59f83511fcc761e
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:57:45 +0430

    add TicketStatusEnum enum - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/Base/Enums/TicketStatusEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace C1System.DataLayar.Entities.Base.Enums;
+
+public enum TicketStatusEnum
+{
+    SystemClosed = 1,
+    Answered = 2,
+    Opened = 3,
+    Pending = 4,
+    Closed = 5,
+}
