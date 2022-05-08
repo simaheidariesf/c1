1a4d8f01f67dabd94844adcb15307be127c2f22f
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:59:12 +0430

    add ICustomerSpeechService interface- 1401/02/18 23:45

---
+++ C1System.Core/Services/Interface/ICustomerSpeechService.cs
@@ -0,0 +1,18 @@
+﻿using C1System.DataLayar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1System.Core.Services.Interface
+{
+    public interface ICustomerSpeechService
+    {
+        List<CustomerSpeech> GetAllCustomerSpeech();
+        CustomerSpeech GetCustomerSpeechById(Guid id);
+        bool AddCustomerSpeech(CustomerSpeech customerSpeech);
+        bool DeleteCustomerSpeech(CustomerSpeech customerSpeech);
+        bool UpdateCustomerSpeech(CustomerSpeech customerSpeech);
+    }
+}
