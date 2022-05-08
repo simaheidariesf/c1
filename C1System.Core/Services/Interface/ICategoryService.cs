10ebf4aaa3702696d6ff1028e5391d633b7ea6ba
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:58:44 +0430

    add ICategoryService interface- 1401/02/18 23:45

---
+++ C1System.Core/Services/Interface/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using C1System.DataLayar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1System.Core.Services.Interface
+{
+    public interface ICategoryService
+    {
+        List<Category> GetAllCategory();
+        Category GetCategoryById(Guid id);
+        bool AddCategory(Category category);
+        bool DeleteCategory(Category category);
+        bool UpdateCategory(Category category);
+    }
+}
