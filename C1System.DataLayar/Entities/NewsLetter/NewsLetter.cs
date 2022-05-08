f10686b349a2c40b04037c7d9166406de2477f0f
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:50:07 +0430

    add NewsLetter entity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/NewsLetter/NewsLetter.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities.NewsLetter;
+
+public class NewsLetter
+{
+    [Key] 
+    public Guid NewsLetterId { get; set; }
+    
+    [Display(Name = "نام و نام خانوادگی")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(250, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string FullName { get; set; }
+    
+    [Display(Name = "ایمیل")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(300, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [EmailAddress(ErrorMessage = "لطفا {0} معتبر وارد کنید.")]
+    public string Email { get; set; }
+}
