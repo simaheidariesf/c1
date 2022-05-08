2b9aa76f77792e5f442c9440e4db04824472444c
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:49:09 +0430

    add FaqCategory entity - 1401/02/18  23:45

---
+++ C1System.DataLayar/Entities/Faq/FaqCategory.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities.Faq;
+
+public class FaqCategory
+{
+    [Key] 
+    public Guid FaqCategoryId { get; set; }
+    
+    [Display(Name = "عنوان دسته بندی")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(200, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string FaqCategoryTitle { get; set; }
+    
+    [Display(Name ="تصویر دسته بندی")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    public string FaqCategoryImage { get; set; }
+}
