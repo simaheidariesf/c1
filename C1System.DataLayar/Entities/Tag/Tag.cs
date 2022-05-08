77292960f0e0b744fcfe63e5844c501c81635492
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:47:54 +0430

    add Tag entity - 1401/02/18  23:45

---
+++ C1System.DataLayar/Entities/Tag/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities;
+
+public class Tag
+{
+    [Key]
+    public Guid TagId { get; set; }
+    
+    [Display(Name ="عنوان تگ")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [MinLength(30 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(150 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Title { get; set; }
+    
+    [Display(Name ="لینک تگ")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [MinLength(150 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    public string Link { get; set; }
+} 
