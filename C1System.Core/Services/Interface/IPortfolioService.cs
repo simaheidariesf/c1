579becb4d651359cd67d91361a315fb99d049fec
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:59:49 +0430

    add IPortfolioService interface- 1401/02/18 23:45

---
+++ C1System.Core/Services/Interface/IPortfolioService.cs
@@ -0,0 +1,18 @@
+﻿using C1System.DataLayar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1System.Core.Services.Interface
+{
+    public interface IPortfolioService
+    {
+        List<Portfolio> GetAllPortfolio();
+        Portfolio GetPortfolioById(Guid id);
+        bool AddPortfolio(Portfolio portfolio);
+        bool DeletePortfolio(Portfolio portfolio);
+        bool UpdatePortfolio(Portfolio portfolio);
+    }
+}
