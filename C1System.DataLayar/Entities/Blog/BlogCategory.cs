3bfebc31002633bc19f29e2121e36ba3c83632e6
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:56:46 +0430

    add BlogCategory entity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/Blog/BlogCategory.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using C1System.DataLayar.Entities.Base;
+
+namespace C1System.DataLayar.Entities.Blog;
+
+public class BlogCategory : BaseEntity
+{
+    [Key]
+    public int BlogCategoryId { get; set; }
+    
+    [Display(Name ="عنوان دسته بندی پست")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [MinLength(20 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(300 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Title { get; set; }
+}
