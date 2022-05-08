2a9f85e12d82960a0982c9b53f79c60592c0679c
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:48:50 +0430

    add Language entity - 1401/02/18  23:45

---
+++ C1System.DataLayar/Entities/Language/Language.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System.DataLayar.Entities.Language;
+
+public class Language
+{
+    [Key] 
+    [Display(Name = "سمبل زبان")]
+    [StringLength(5)] 
+    public string Symbol { get; set; } = "fa-IR";
+    
+    [Display(Name = "نام زبان")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(50, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string LanguageName { get; set; }
+}
