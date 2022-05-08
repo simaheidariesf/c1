1f257618f18501e8be43c871f0d644f8edc7740f
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:48:26 +0430

    add NewsLetter entity - 1401/02/18  23:45

---
+++ C1System.DataLayar/Entities/Geo/Province.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System.DataLayar.Entities.Geo;
+
+public class Province
+{
+    [Key]
+    [Display(Name = "شناسه")]
+    public int ProvinceId { get; set; }
+    
+    [StringLength(50)]
+    [Display(Name = "نام استان")]
+    public string ProvinceName { get; set; }
+    
+    [Display(Name = "کشور")]
+    public int CountryId { get; set; }
+
+    [ForeignKey(nameof(CountryId))]
+    public virtual Country Country { get; set; }
+
+    public virtual ICollection<City> Cities { get; set; }
+}
