b9b3c5ccb17616ce1ea22d2b16a842d70678114c
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:54:32 +0430

    add Contact entity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/Contact/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities.Contact;
+
+public class Contact
+{
+    [Key] 
+    public Guid ContactId { get; set; }
+    
+    [Display(Name = "نام و نام خانوادگی")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(200, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string FullName { get; set; }
+    
+    [Display(Name = "شماره موبایل")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [MinLength(11, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(11, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string MobileNumber { get; set; }
+    
+    [Display(Name = "ایمیل")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(300, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [EmailAddress(ErrorMessage = "لطفا {0} معتبر وارد کنید.")]
+    public string Email { get; set; }
+    
+    [Display(Name = "توضیحات")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
+    public string Description { get; set; }
+}
