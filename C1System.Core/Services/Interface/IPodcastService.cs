c259c6edd3f23b5a4fcb603eccfcdafe98ca06c7
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:59:34 +0430

    add IPodcastService interface- 1401/02/18 23:45

---
+++ C1System.Core/Services/Interface/IPodcastService.cs
@@ -0,0 +1,18 @@
+﻿using C1System.DataLayar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1System.Core.Services.Interface
+{
+    public interface IPodcastService
+    {
+        List<Podcast> GetAllTag();
+        Podcast GetPodcastById(Guid id);
+        bool AddPodcastg(Podcast podcast);
+        bool DeleteTag(Podcast podcast);
+        bool UpdateTag(Podcast podcast);
+    }
+}
