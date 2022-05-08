4c90e2f82e1ea921e36cf95063e0ea4a8998ef0c
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:51:32 +0430

    add NeedsAssessment entity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/NeedsAssessment/NeedsAssessment.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities.NeedsAssessment;
+
+public class NeedsAssessment
+{
+    [Key]
+    public Guid NeedsAssessmentId { get; set; }
+    
+    [Display(Name ="نام و نام خانوادگی")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید.")]
+    [MinLength(10 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(80 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string FullName { get; set; }
+    
+    //todo ارتباط با جدول Category
+    //todo ارتباط با جدول Category برای زیر دسته ها
+
+    [Display(Name ="ایمیل")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [EmailAddress(ErrorMessage = "لطفا {0} معتبر وارد کنید")]
+    [MinLength(50 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(1500 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Email { get; set; }
+    
+    [Display(Name ="شماره موبایل")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [Phone(ErrorMessage = "لطفا {0} معتبر وارد کنید")]
+    [MinLength(11 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(11 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string PhoneNumber { get; set; }
+
+    [Display(Name = "متن پیام")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
+    public string Note { get; set; }
+    
+    [Display(Name = "فایل پیوست")]
+    public string Media { get; set; }
+}
