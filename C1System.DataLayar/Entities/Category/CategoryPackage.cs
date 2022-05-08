6c54b65e41cfe73dea45b8f3ff2cc7ac60ef6fea
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:56:06 +0430

    add CategoryPackage entity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/Category/CategoryPackage.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities;
+
+public class CategoryPackage
+{
+    [Key]
+    public Guid CategoryId { get; set; }
+    
+    [Display(Name ="نام پکیج")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [MinLength(5 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(80 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Title { get; set; }
+    
+    [Display(Name = "زیرعنوان پکیج")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
+    [MinLength(5, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(150, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string SubTitle { get; set; }
+    
+    //todo ارتباط با لیست اتم ها اضافه شود 
+}
