1864732b711afec88147a468f5613147a1b67420
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-09 00:01:34 +0430

    add SliderService - 1401/02/18 23:45

---
+++ C1System.Core/Services/SliderService.cs
@@ -0,0 +1,85 @@
+﻿using C1System.Core.Services.Interface;
+using C1System.DataLayar.Context;
+using C1System.DataLayar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1System.Core.Services
+{
+    public class SliderService : ISliderService
+    {
+        private readonly C1SystemContext _context;
+        public SliderService(C1SystemContext context)
+        {
+            _context = context;
+        }
+        public bool AddSlider(Slider slider)
+        {
+            try
+            {
+                _context.Sliders.Add(slider);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool DeleteSlider(Slider slider)
+        {
+            if (slider != null)
+            {
+                try
+                {
+                    _context.Sliders.Remove(slider);
+                    _context.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    return false;
+                }
+            }
+            else
+                return false;
+
+        }
+
+        public List<Slider> GetAllSlider()
+        {
+            return _context.Sliders.OrderBy(x=> x.SliderId).ToList();
+        }
+
+        public Slider GetSliderById(Guid id)
+        {
+            return _context.Sliders.Find(id);
+        }
+
+        public bool UpdateSlider(Slider slider)
+        {
+            if(slider != null)
+            {
+                try
+                {
+                    _context.Sliders.Update(slider);
+                    _context.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            else
+                return false;
+        }
+    }
+}
