8e19e73a03adabb985e710dbe2dcc876db2e0634
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:50:46 +0430

    add UserRole entity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/User/UserRole.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System.DataLayar.Entities;
+
+public class UserRole
+{
+    [Key]
+    public int RoleId { get; set; }
+
+    public string RoleName { get; set; }
+}
