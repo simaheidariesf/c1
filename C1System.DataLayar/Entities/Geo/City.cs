57c81254ce10464f25a8f1befc1fcc9134b02fcd
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:48:17 +0430

    add City entity - 1401/02/18  23:45

---
+++ C1System.DataLayar/Entities/Geo/City.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System.DataLayar.Entities.Geo;
+
+public class City
+{
+    [Key]
+    [Display(Name = "شناسه")]
+    public int CityId { get; set; }
+    
+    [StringLength(50)]
+    [Display(Name = "شهر")]
+    public string CityName { get; set; }
+    
+    [Display(Name = "استان")]
+    public int ProvinceId { get; set; }
+
+    [ForeignKey(nameof(ProvinceId))]
+    public virtual Province Province { get; set; }
+}
