307d9e0686077192cb3550bd767eb005e7eb804c
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-09 00:00:17 +0430

    add ITagService interface- 1401/02/18 23:45

---
+++ C1System.Core/Services/Interface/ITagService.cs
@@ -0,0 +1,18 @@
+﻿using C1System.DataLayar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1System.Core.Services.Interface
+{
+    public interface ITagService 
+    {
+        List<Tag> GetAllTag();
+        Tag GetTagById(Guid id);
+        bool AddTag(Tag tag);
+        bool DeleteTag(Tag tag);
+        bool UpdateTag(Tag tag);
+    }
+}
