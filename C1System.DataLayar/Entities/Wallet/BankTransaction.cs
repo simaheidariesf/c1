4b9f5470cf18b8212f8c54cf7766b7e18bec5fd3
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:53:26 +0430

    add BankTransaction entity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/Wallet/BankTransaction.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System.DataLayar.Entities.Wallet;
+
+public class BankTransaction
+{
+    [Key]
+    public int BankTransactionId { get; set; }
+    
+    [Column(TypeName = "datetime")]
+    public DateTime? CreateDateTime { get; set; }
+
+    [Column("order_id")]
+    [StringLength(50)]
+    public string OrderId { get; set; }
+    
+    [Column("amount", TypeName = "decimal(18, 0)")]
+    public decimal? Amount { get; set; }
+    
+    [Column("OK")]
+    public bool Ok { get; set; }
+}
