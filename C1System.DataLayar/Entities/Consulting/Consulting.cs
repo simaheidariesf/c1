deeb341c1a13a17f4d15d2c768e8d0306ef2a087
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:55:15 +0430

    add Consulting entity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/Consulting/Consulting.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DefaultNamespace;
+
+public class Consulting
+{
+    [Key] 
+    public Guid ConsultingId { get; set; }
+    
+    [Display(Name = "نام و نام خانوادگی")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(200, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string FullName { get; set; }
+    
+    [Display(Name = "شماره موبایل")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [MinLength(11, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(11, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string MobileNumber { get; set; }
+}
