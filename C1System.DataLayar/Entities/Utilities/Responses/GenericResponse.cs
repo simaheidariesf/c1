6b1c8c94dd20bc930c0f19ff1ba4a942d692db3e
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:53:42 +0430

    add GenericResponse - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/Utilities/Responses/GenericResponse.cs
@@ -0,0 +1,28 @@
+﻿using C1System.DataLayar.Entities.Utilities.Enums;
+
+namespace C1System.DataLayar.Entities.Responses;
+
+public class GenericResponse<T> : GenericResponse {
+    public T? Result { get; set; }
+
+    public GenericResponse(T result, UtilitiesStatusCodes status = UtilitiesStatusCodes.Success, string message = "") {
+        Result = result;
+        Status = status;
+        Message = message;
+    }
+}
+
+public class GenericResponse {
+    public UtilitiesStatusCodes Status { get; set; }
+    public string Message { get; set; }
+    public List<Guid>? Ids { get; set; }
+    public Guid? Id { get; set; }
+
+    public GenericResponse(UtilitiesStatusCodes status = UtilitiesStatusCodes.Success, string message = "", List<Guid>? ids = null,
+        Guid? id = null) {
+        Status = status;
+        Message = message;
+        Ids = ids;
+        Id = id;
+    }
+}
