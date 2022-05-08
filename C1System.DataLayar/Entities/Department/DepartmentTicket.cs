37bb10be92df151743bddd2febfb5f686570d01c
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:49:44 +0430

    add DepartmentTicket entity - 1401/02/18  23:45

---
+++ C1System.DataLayar/Entities/Department/DepartmentTicket.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities.Department;
+
+public class DepartmentTicket
+{
+    [Key] 
+    public int DepartmentTicketId { get; set; }
+    
+    [Display(Name = "نام دپارتمان")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(50, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string DepartmentTicketName { get; set; }
+    
+    [Display(Name = "متن معرفی")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(500, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string DepartmentTicketIntro { get; set; }
+}
