ac7f536f731612acfe11282d52570df2348cfad0
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-09 00:01:42 +0430

    add TagService - 1401/02/18 23:45

---
+++ C1System.Core/Services/TagService.cs
@@ -0,0 +1,85 @@
+﻿using C1System.DataLayar.Context;
+using C1System.DataLayar.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1System.Core.Services.Interface
+{
+    public class TagService : ITagService
+    {
+        private readonly C1SystemContext _context;
+        public TagService(C1SystemContext context)
+        {
+            _context = context;
+        }
+
+        public bool AddTag(Tag tag)
+        {
+            try
+            {
+                _context.Tags.Add(tag);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool DeleteTag(Tag tag)
+        {
+            if (tag != null)
+            {
+                try
+                {
+                    _context.Tags.Remove(tag);
+                    _context.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    return false;
+                }
+            }
+            else
+                return false;
+        }
+
+        public List<Tag> GetAllTag()
+        {
+            return _context.Tags.ToList();
+        }
+
+        public Tag GetTagById(Guid id)
+        {
+            return _context.Tags.Find(id);
+        }
+
+        public bool UpdateTag(Tag tag)
+        {
+            if (tag != null)
+            {
+                try
+                {
+                    _context.Tags.Update(tag);
+                    _context.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            else
+                return false;
+        }
+    }
+}
