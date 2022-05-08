d6596a7fd1195404237e93cf98432a959820dad3
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:47:11 +0430

    add DemoPortfolio entity - 1401/02/18  23:45

---
+++ C1System.DataLayar/Entities/Portfolio/DemoPortfolio.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities;
+
+public class DemoPortfolio
+{
+    [Key]
+    public Guid DemoPortfolioId { get; set; }
+    
+    [Display(Name ="عنوان دمو")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [MinLength(5 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(150 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Title { get; set; }
+    
+    [Display(Name = "توضیحات")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
+    public string Desc { get; set; }
+    
+    [Display(Name = "ویدئو")]
+    public string Video { get; set; }
+    
+    [Display(Name = "عکس کاور ویدئو")]
+    public string CoverVideo { get; set; }
+}
