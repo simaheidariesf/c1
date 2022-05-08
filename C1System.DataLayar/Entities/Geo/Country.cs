01048412cf317ac83024d28966ea3c597f4597be
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:48:42 +0430

    add Country entity - 1401/02/18  23:45

---
+++ C1System.DataLayar/Entities/Geo/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System.DataLayar.Entities.Geo;
+
+public class Country
+{
+    [Key]
+    [Display(Name = "شناسه")]
+    public int CountryId { get; set; }
+    
+    [StringLength(200)]
+    [Display(Name = "نام کشور")]
+    public string CountryName { get; set; }
+}
