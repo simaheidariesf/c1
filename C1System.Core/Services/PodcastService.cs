7fa3c07306a2121ca1c5f1ac9d7e3c8d90e44372
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-09 00:00:59 +0430

    add PodcastService- 1401/02/18 23:45

---
+++ C1System.Core/Services/PodcastService.cs
@@ -0,0 +1,85 @@
+﻿using C1System.Core.Services.Interface;
+using C1System.DataLayar.Context;
+using C1System.DataLayar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1System.Core.Services
+{
+    public class PodcastService : IPodcastService
+    {
+        private readonly C1SystemContext _context;
+        public PodcastService(C1SystemContext context)
+        {
+            _context = context;
+        }
+
+        public bool AddPodcastg(Podcast podcast)
+        {
+            try
+            {
+                _context.Podcasts.Add(podcast);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool DeleteTag(Podcast podcast)
+        {
+            if (podcast != null)
+            {
+                try
+                {
+                    _context.Podcasts.Remove(podcast);
+                    _context.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    return false;
+                }
+            }
+            else
+                return false;
+        }
+
+        public List<Podcast> GetAllTag()
+        {
+            return _context.Podcasts.ToList();
+        }
+
+        public Podcast GetPodcastById(Guid id)
+        {
+            return _context.Podcasts.Find(id);
+        }
+
+        public bool UpdateTag(Podcast podcast)
+        {
+            if (podcast != null)
+            {
+                try
+                {
+                    _context.Podcasts.Update(podcast);
+                    _context.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            else
+                return false;
+        }
+    }
+}
