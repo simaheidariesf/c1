a26d26cd931bd7611941e8170586f64a8ff4d6ac
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:51:24 +0430

    add TechnologyPortfolio entity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/Portfolio/TechnologyPortfolio.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities;
+
+public class TechnologyPortfolio
+{
+    [Key]
+    public int TechnologyPortfolioId { get; set; }
+    
+    [Display(Name ="نام تکنولوژی")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [MinLength(5 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(150 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Title { get; set; }
+    
+    [Display(Name = "تصویر تکنولوژی")]
+    public string TechnologyImage { get; set; }
+}
