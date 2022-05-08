d7b5f837aba72879b45c94d0757bacf032dbb7b9
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:49:37 +0430

    add Department entity - 1401/02/18  23:45

---
+++ C1System.DataLayar/Entities/Department/Department.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities.Department;
+
+public class Department
+{
+    [Key] 
+    public int DepartmentId { get; set; }
+    
+    [Display(Name = "نام دپارتمان")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(50, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string DepartmentName { get; set; }
+    
+    [Display(Name = "متن معرفی")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(500, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string DepartmentIntro { get; set; }
+}
