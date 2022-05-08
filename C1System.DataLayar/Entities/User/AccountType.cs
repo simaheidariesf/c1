f09572132e94a8bb146161c34dee25ef51871c1b
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:51:45 +0430

    add AccountType entity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/User/AccountType.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System.DataLayar.Entities;
+
+public class AccountType
+{
+    [Key]
+    public int AccountTypeId { get; set; }
+
+    [Display(Name ="نوع کاربری")]
+    public string AccountTypeName { get; set; }
+    
+    #region Relation
+    // public ICollection<User> Users { get; set; }
+    #endregion
+}
