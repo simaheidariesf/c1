e38a2e30f8e2153a678448e5a2440ebe1a7bd8a8
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:49:51 +0430

    add Message entity - 1401/02/18  23:45

---
+++ C1System.DataLayar/Entities/Message/Message.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using C1System.DataLayar.Entities.Base;
+
+namespace C1System.DataLayar.Entities.Message;
+
+public class Message : BaseEntity
+{
+    [Key] 
+    public Guid MessageId { get; set; }
+    
+    [Display(Name = "عنوان پیام")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(50, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string MessageTitle { get; set; }
+    
+    [Display(Name = "متن پیام")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(500, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string MessageDesc { get; set; }
+
+    [Display(Name = "خوانده شده یا نشده")]
+    public bool? IsRead { get; set; }
+    
+    //todo ارتباط با جدول Department
+}
