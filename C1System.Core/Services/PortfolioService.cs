7382023df099f575188a98275f2a0b71438201ce
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-09 00:01:14 +0430

    add PortfolioService - 1401/02/18 23:45

---
+++ C1System.Core/Services/PortfolioService.cs
@@ -0,0 +1,87 @@
+﻿using C1System.Core.Services.Interface;
+using C1System.DataLayar.Context;
+using C1System.DataLayar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1System.Core.Services
+{
+    public class PortfolioService : IPortfolioService
+    {
+        private C1SystemContext _context;
+        public PortfolioService(C1SystemContext context)
+        {
+            _context = context;
+        }
+
+        public List<Portfolio> GetAllPortfolio()
+        {
+            return _context.Portfolios.ToList();
+        }
+
+        public Portfolio GetPortfolioById(Guid id)
+        {
+            return _context.Portfolios.Find(id);
+        }
+
+        public bool AddPortfolio(Portfolio portfolio)
+        {
+            try
+            {
+                _context.Portfolios.Add(portfolio);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool DeletePortfolio(Portfolio portfolio)
+        {
+            if (portfolio != null)
+            {
+                try
+                {
+                    _context.Portfolios.Remove(portfolio);
+                    _context.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    return false;
+                }
+            }
+            else
+                return false;
+        }
+
+        public bool UpdatePortfolio(Portfolio portfolio)
+        {
+            if (portfolio != null)
+            {
+                try
+                {
+                    _context.Portfolios.Update(portfolio);
+                    _context.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            else
+                return false;
+        }
+
+        
+    }
+}
