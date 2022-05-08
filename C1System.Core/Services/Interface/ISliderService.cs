1006a8f32961caf4b9f08d9caa515f5c7f2c9865
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-09 00:00:09 +0430

    add ISliderService interface- 1401/02/18 23:45

---
+++ C1System.Core/Services/Interface/ISliderService.cs
@@ -0,0 +1,18 @@
+﻿using C1System.DataLayar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1System.Core.Services.Interface
+{
+    public interface ISliderService
+    {
+        List<Slider> GetAllSlider();
+        Slider GetSliderById(Guid id);
+        bool AddSlider(Slider slider);
+        bool DeleteSlider(Slider slider);
+        bool UpdateSlider(Slider slider);
+    }
+}
