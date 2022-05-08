967b33a25a78ab8ee6c420e1f4e72a1a3b24ca51
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 23:56:23 +0430

    add CareerOpportunity entity - 1401/02/18 23:45

---
+++ C1System.DataLayar/Entities/CareerOpportunity/CareerOpportunity.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities.CareerOpportunity;
+
+public class CareerOpportunity
+{
+    [Key] 
+    public Guid CareerOpportunityId { get; set; }
+    
+    [Display(Name = "عنوان فرصت شغلی")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(300, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string Title { get; set; }
+    
+    [Display(Name = "تصویر شاخص")]
+    public string FeatureMedia { get; set; }
+    
+    [Display(Name = "توضیحات")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
+    [MinLength(200, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(1500, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Description { get; set; }
+}
