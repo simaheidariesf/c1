de96184d50b5386628466343b029a1639697f033
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-09 00:00:32 +0430

    add IUserService interface- 1401/02/18 23:45

---
+++ C1System.Core/Services/Interface/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using C1System.DataLayar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1System.Core.Services.Interface
+{
+    public interface IUserService
+    {
+        List<User> GetAllUser();
+        User GetUserById(Guid id);
+        bool AddUser(User user);
+        bool DeleteUser(User user);
+        bool UpdateUser(User user);
+    }
+}
